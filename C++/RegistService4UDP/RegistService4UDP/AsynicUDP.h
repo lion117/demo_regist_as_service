187a7687d682bd4bfe44bc970ef240434ae83dba
lion <ngixpr@qq.com>
2016-05-30 16:44:00 +0800

    1 add  multpl thread to support udp

--- C++/RegistService4UDP/RegistService4UDP/AsynicUDP.h
+++ C++/RegistService4UDP/RegistService4UDP/AsynicUDP.h
@@ -6,19 +6,11 @@ using namespace std;
 #include "Poco/SharedPtr.h"
 #include "Poco/Runnable.h"
 #include "Poco/Thread.h"
-#include "Poco/Net/NetException.h"
-
+#include "Poco/Threadpool.h"
+#include "Poco/Mutex.h"
 #include <functional>
+#include <queue>
 
-
-void onRecieve11(Poco::Net::SocketAddress t_address, string t_data);
-
-inline void onRecieve11(Poco::Net::SocketAddress t_address,  string t_data)
-{
-	string i_echo = "echo:  " + t_data;
-	cout << i_echo << endl;
-	//_udp_socket->sendTo(i_echo.c_str(), i_echo.size(), t_address);
-}
 
 
 class UDPListener 
@@ -32,12 +24,21 @@ protected:
 
 class AsynicUDP : public Poco::Runnable
 {
+
+	class Packet
+	{
+	public:
+		Poco::Net::SocketAddress address;
+		string  data;
+	};
+
 public:
 	AsynicUDP(UDPListener * ptr_obj)
 	{
 		_ptr_listener = ptr_obj;
 		_udp_socket = new Poco::Net::DatagramSocket();
 		_is_running = false;
+		_is_setup = false;
 	};
 
 	~AsynicUDP()
@@ -70,17 +71,20 @@ public:
 	bool start()
 	{
 		_is_running = true;
-		_thread.start(*this);
-		return false;
+		_thread_pool.start(*this);
+		Poco::Thread::sleep(10);  // ensure the first thread would setup 
+
+		for (int i = 0; i < 20; i++)
+		{
+			_thread_pool.start(*this);  // setup the worker thread 
+		}
+		return true;
 	}
 
 	void stop()
 	{
 		_is_running = false;
-		if (_thread.isRunning())
-		{
-			_thread.join();
-		}
+		_thread_pool.joinAll();
 	}
 
 	bool sendTo(const Poco::Net::SocketAddress &t_address , const string & t_data)
@@ -93,14 +97,32 @@ public:
 	}
 
 	virtual void onRecieve(Poco::Net::SocketAddress t_address, string t_data){}
+	void setCallbck(function<void(Poco::Net::SocketAddress, string)> t_callback)
+	{
+		_delegate_callback = t_callback;
+	}
 
 
 protected:
 	void run()
 	{
-		while (_is_running)
+		if (! _is_setup)
 		{
-			char i_buff[2048] = {0};
+			epoll();
+		}
+		/// for the worker thread 
+		else
+		{
+			msgWorker();
+		}
+	}
+
+
+	void epoll()
+	{
+		_is_setup = true;
+		while (true){
+			char i_buff[2048] = { 0 };
 			Poco::Net::SocketAddress i_peeraddress;
 			try
 			{
@@ -110,19 +132,39 @@ protected:
 					continue;
 				}
 				string i_data(i_buff, recv_lenth);
-				onRecieve(i_peeraddress, i_data);
-				if(_delegate_callback){
-					_delegate_callback(i_peeraddress, i_data);
-				}
 
 
+				Packet i_packet;
+				i_packet.address = i_peeraddress;
+				i_packet.data = i_data;
+				_data_queue.push(i_packet);
 			}
 			catch (Poco::Exception ex)
 			{
-				cout<< ex.message()<<endl;
+				cout << ex.message() << endl;
 			}
-			
 		}
+	}
+
+	void msgWorker()
+	{
+		while(_is_running)
+		{
+			Packet i_packet;
+			{
+				Poco::Mutex::ScopedLock i_lock(_lock);
+				if (_data_queue.size() == 0)
+				{
+					continue;
+				}
+				i_packet = _data_queue.front();
+				_data_queue.pop();
+			}
+			onRecieve(i_packet.address, i_packet.data);
+			if (_delegate_callback){
+				_delegate_callback(i_packet.address, i_packet.data);
+			}
+		}	
 	}
 
 
@@ -131,17 +173,18 @@ protected:
 private:
 	UDPListener * _ptr_listener;
 	Poco::SharedPtr<Poco::Net::DatagramSocket> _udp_socket;
-	Poco::Thread  _thread;
 	bool _is_running;
 	function<void(Poco::Net::SocketAddress, string)> _delegate_callback;
+	Poco::ThreadPool _thread_pool;
+	Poco::Mutex  _lock;
+	queue<Packet> _data_queue;
+	bool          _is_setup;
 
 
 public:
 
-	void setCallbck(function<void(Poco::Net::SocketAddress, string)> t_callback)
-	{
-		_delegate_callback = t_callback;
-	}
+
+
 
 	static void main()
 	{
@@ -152,18 +195,18 @@ public:
 			cout << "adsress already in use " << endl;
 			return;
 		}
-		i_this.setCallbck(onRecieve11);
-		i_this.start();
-		system("pause");
-		string i_data = "1234567890";
-		for (int i = 0; i < 10; i++)
-		{
-			i_data += i_data;
-		}
+		//i_this.setCallbck(onRecieve11);
+		//i_this.start();
+		//system("pause");
+		//string i_data = "1234567890";
+		//for (int i = 0; i < 10; i++)
+		//{
+		//	i_data += i_data;
+		//}
 
-		i_this.sendTo(Poco::Net::SocketAddress("127.0.0.1", 8082), i_data);
-		system("pause");
-		
+		//i_this.sendTo(Poco::Net::SocketAddress("127.0.0.1", 8082), i_data);
+		//system("pause");
+		//
 	}
 
 
